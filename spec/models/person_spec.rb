3f5766dce38491d714cafca568cb3751451b0051
matias acuna <agroang@gmail.com>
2022-01-24 17:40:24 +0900

    added the first test on rspec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+# require './app/models/person' I don't need this as I am using the testing for Person here
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should get rid of both leading and trailing whitespaces' do
+    test_guy = Person.new(first_name: 'Peter', last_name: 'Rock', age: 35)
+    expect(test_guy.delete_surrounding_whitespaces('  testing example         ')).to eq('testing example')
+    expect(test_guy.delete_surrounding_whitespaces('          will this work!?   ')).to eq('will this work!?')
+  end
 end
