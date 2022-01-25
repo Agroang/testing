a1fff87e17c73c6a6aa21e6c5754be9c7344c270
matias acuna <agroang@gmail.com>
2022-01-25 18:41:29 +0900

    refactored the test a little bit, created the instance at the beginning -tried let to no avail- the added a small test for a module method included in the Person class

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,9 +4,16 @@ require 'rails_helper'
 # require './app/models/person' I don't need this as I am using the testing for Person here
 
 RSpec.describe Person, type: :model do
+  test_guy = Person.new(first_name: 'Peter', last_name: 'Rock', age: 35)
+
+  # test an instance method inside the person class
   it 'should get rid of both leading and trailing whitespaces' do
-    test_guy = Person.new(first_name: 'Peter', last_name: 'Rock', age: 35)
     expect(test_guy.delete_surrounding_whitespaces('  testing example         ')).to eq('testing example')
     expect(test_guy.delete_surrounding_whitespaces('          will this work!?   ')).to eq('will this work!?')
+  end
+
+  # tests a module method inside the Person class
+  it 'should return the string hello' do
+    expect(test_guy.hello).to eq('hello')
   end
 end
