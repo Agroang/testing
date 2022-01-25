bac11bc32651a40b80deff4bab78e4f4671f5127
matias acuna <agroang@gmail.com>
2022-01-25 18:10:14 +0900

    added a module to the class for testing purposes

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,8 @@
 
 # class to test different methods
 class Person < ApplicationRecord
+  # this should allow the instances of Person to use the hello method (foo module)
+  include Foo
   def delete_surrounding_whitespaces(string)
     string.strip
   end
