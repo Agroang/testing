f07db46ab23590b5993f9767a23e61539ecc6c73
matias acuna <agroang@gmail.com>
2022-02-01 18:15:10 +0900

    added a test to check if the module is inluded

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -7,18 +7,23 @@ RSpec.describe Person, type: :model do
   test_guy = Person.new(first_name: 'Peter', last_name: 'Rock', age: 35)
 
   # test an instance method inside the person class
-  it 'should get rid of both leading and trailing whitespaces' do
+  it 'gets rid of both leading and trailing whitespaces' do
     expect(test_guy.delete_surrounding_whitespaces('  testing example         ')).to eq('testing example')
     expect(test_guy.delete_surrounding_whitespaces('          will this work!?   ')).to eq('will this work!?')
   end
 
   # tests a module method inside the Person class
-  it 'should return the string hello' do
+  it 'returns the string hello' do
     expect(test_guy.hello).to eq('hello')
   end
 
   # tests the full_name method to return the full name
-  it 'should return the full of the instance as first_name last_name' do
+  it 'returns the full of the instance as first_name last_name' do
     expect(test_guy.full_name(test_guy.first_name, test_guy.last_name)).to eq('Peter Rock')
+  end
+
+  # test if the instance has the module inside
+  it 'test if the instance has the module inside' do
+    expect(test_guy).to be_a_kind_of(Foo) # Foo is a module included in Person
   end
 end
